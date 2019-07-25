f460d69d0238f4ee19131f71b49305fae90daa2d
gw <2132>
2019-07-25 14:01:20 +0800

    add: 新增层数与楞型枚举数据

--- src/utils/system/enum/index.js
+++ src/utils/system/enum/index.js
@@ -1,4 +1,51 @@
 import MapObject from './mapObject';
+const fluteTypeLayer = [
+  {
+    label: '3层',
+    fluteType: [
+      {label: '单A瓦'},
+      {label: '单B瓦'},
+      {label: '单C瓦'},
+      {label: '单E瓦'},
+      {label: '单F瓦'}
+    ]
+  },
+  {
+    label: '5层',
+    fluteType: [
+      {label: 'AA瓦'},
+      {label: 'AB瓦'},
+      {label: 'AC瓦'},
+      {label: 'AE瓦'},
+      {label: 'BB瓦'},
+      {label: 'BC瓦'},
+      {label: 'BE瓦'},
+      {label: 'CC瓦'},
+      {label: 'CE瓦'},
+      {label: 'EE瓦'}
+    ]
+  },
+  {
+    label: '7层',
+    fluteType: [
+      {label: 'BAA瓦'},
+      {label: 'BAB瓦'},
+      {label: 'BAC瓦'},
+      {label: 'BCA瓦'},
+      {label: 'BCB瓦'},
+      {label: 'BCC瓦'},
+      {label: 'BEE瓦'},
+      {label: 'CAC瓦'},
+      {label: 'CEB瓦'},
+      {label: 'EBA'},
+      {label: 'EBC瓦'}
+    ]
+  }
+];
+const fluteType = fluteTypeLayer.reduce((arr, obj)=>{
+  return arr.concat(obj.fluteType.map(obj=>({ lable: obj.label, value: obj.label })));
+}, [{label: '全部', value: ''}]);
+
 export default {
   orderTip: new MapObject({
     'all': {
@@ -73,5 +120,25 @@ export default {
       label: '异常补单',
       value: '4'
     }
-  })
+  }),
+  linePressingWay: new MapObject({
+    'not': {
+      label: '不压线',
+      value: '0'
+    },
+    'normal': {
+      label: '普通压线',
+      value: '1'
+    },
+    'inner': {
+      label: '内压线',
+      value: '2'
+    },
+    'transverse': {
+      label: '横压线',
+      value: '3'
+    }
+  }),
+  fluteTypeLayer,
+  fluteType
 };
