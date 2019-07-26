5ebd7d040941e983dfd65c5e10750e96bfe02567
zhibi <1017866168@qq.com>
2019-07-26 17:32:37 +0800

    mod 完成生产线设置50%

---
+++ src/views/baseSetting/components/bs-card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bs-card">
+    <div class="bs-card-header">{{title}}</div>
+    <div class="bs-card-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bsCard',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+  .bs-card{
+    width: 100%;
+    &-header{
+      font-size: 18px;
+      font-weight: bold;
+      background: #fafafa;
+      color: #666;
+      padding: 20px;
+    }
+    &-content{
+      padding-left: 10px;
+      padding-top: 20px;
+    }
+  }
+</style>
