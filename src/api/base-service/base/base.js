b000908db269f4249aecf2b4ef39b626c4d6362d
gw <2132>
2019-08-16 16:09:36 +0800

    fix: 解决http请求策略没有生效的bug

--- src/api/base-service/base/base.js
+++ src/api/base-service/base/base.js
@@ -5,7 +5,7 @@ const apiService = new HttpFactory(httpConfig);
 function baseApi(service, value = {}, method = 'post') {
   let { cancelToken } = value;
   delete value.cancelToken;
-  return apiService[method](service, value, undefined, undefined, cancelToken);
+  return apiService[method](service, value, cancelToken);
 }
 
 export default function baseService(base) {
