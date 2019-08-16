82e1dac7dabf37603ad72acf008c67f2af99a061
zhibi <1017866168@qq.com>
2019-08-16 14:26:07 +0800

    mod 完成订单统计接口对接

--- src/views/statement/orderStatistics.vue
+++ src/views/statement/orderStatistics.vue
@@ -1,18 +1,19 @@
 <template>
-  <single-page class="orderStatistics">
+  <single-page class="orderStatistics" v-loading="loading">
     <dj-search ref="search" :config="searchConfig" @search="search"></dj-search>
     <page-pane>
       <dj-table ref="table"
                 :data="tableData"
                 height="100%"
+                :total="pageTotal"
                 :columns="tableColumns"
                 :column-type="['index']"
                 @update-data="getList">
         <div slot="btn">
           <div class="ext-data">
-            <div class="ext-data-item">订单数: {{extData.orderNum}}</div>
-            <div class="ext-data-item">产品数量(片)：{{extData.prodNum}}</div>
-            <div class="ext-data-item">产品面积(㎡)：{{extData.area}}</div>
+            <div class="ext-data-item">订单数: {{extData.orderAmount}}</div>
+            <div class="ext-data-item">产品数量(片)：{{extData.productAmount}}</div>
+            <div class="ext-data-item">产品面积(㎡)：{{extData.productArea}}</div>
           </div>
         </div>
       </dj-table>
@@ -21,15 +22,15 @@
 </template>
 <script>
   import dayjs from 'dayjs';
-  import orderStatisticsService from '../../api/service/orderStatistics';
+  import orderStatisticsService from '../../api/service/orderManage';
   export default {
-    name: 'orderStatistics',
+    name: 'orderManage',
     data: function () {
       return {
         searchConfig: [
           {
             label: '分发日期', key: 'timeRange', type: 'date', attrs: {
-              default: [dayjs(new Date()).subtract(7, 'day').format('YYYY-MM-DD'), dayjs(new Date()).format('YYYY-MM-DD')],
+              default: [dayjs(new Date()).subtract(45, 'day').format('YYYY-MM-DD'), dayjs(new Date()).format('YYYY-MM-DD')],
               beforeChange: (val) => {
                 let _val = val;
                 if (val[0] && val[1]) {
@@ -42,6 +43,7 @@
                 return _val;
               },
               type: 'daterange',
+              clearable: false,
               pickerOptions: {
                 shortcuts: [
                   {
@@ -75,9 +77,9 @@
               valueFormat: "yyyy-MM-dd"
             }
           },
-          {label: '材料名称', key: 'paperCode', type: 'input'},
+          {label: '材料名称', key: 'materialName', type: 'input'},
           {
-            key: 'fluteType',
+            key: 'tileModels',
             label: '瓦楞楞型',
             type: 'custom',
             attrs: {
@@ -116,18 +118,20 @@
         ],
         tableData: [],
         tableColumns: [
-          {label: '材料名称', prop: 'name'},
-          {label: '瓦楞楞型', prop: 'lengxing'},
-          {label: '订单数', prop: 'orderNum', sortable: true},
-          {label: '产品数量（片）', prop: 'prodNum', sortable: true},
-          {label: '产品面积（m²）', prop: 'area', sortable: true},
+          {label: '材料名称', prop: 'materialName'},
+          {label: '瓦楞楞型', prop: 'tileModel'},
+          {label: '订单数', prop: 'orderAmount', sortable: true, sortMethod: (a, b)=>this.sort(a, b, 'orderAmount')},
+          {label: '产品数量（片）', prop: 'productAmount', sortable: true, sortMethod: (a, b)=>this.sort(a, b, 'productAmount')},
+          {label: '产品面积（m²）', prop: 'productArea', sortable: true, sortMethod: (a, b)=>this.sort(a, b, 'productArea')},
         ],
         searchData: {},
         extData: {
-          orderNum: '',
-          prodNum: '',
-          area: ''
-        }
+          orderAmount: '',
+          productAmount: '',
+          productArea: ''
+        },
+        pageTotal: 0,
+        loading: false
       };
     },
     created() {
@@ -136,14 +140,29 @@
       this.$refs.search.search();
     },
     methods: {
+      sort(a, b, prop) {
+        if (a[prop] && b[prop]) {
+          return a[prop] - b[prop];
+        }
+        console.log(a, b, prop);
+      },
       getList(page) {
+        this.loading = true;
+        const { timeRange, ...restSearchData } = this.searchData;
         let post = {
-          ...this.searchData,
+          startTime: timeRange[0],
+          endTime: timeRange[1],
+          ...restSearchData,
           ...page
         };
-        this.dj_api_extend(orderStatisticsService.list, post).then(res => {
-          this.tableData = res.list
-        })
+        this.dj_api_extend(orderStatisticsService.orderStatistics, post).then(res => {
+          const { orderStatisticList, total, ...restData } = res;
+          this.tableData = orderStatisticList;
+          this.extData = restData;
+          this.pageTotal = Number(total);
+        }).finally(()=>{
+          this.loading = false;
+        });
       },
       search(query) {
         this.searchData = query;
