ca65882c870d306323d570944c65312e520a52d8
zhibi <1017866168@qq.com>
2019-08-29 17:31:17 +0800

    fix 11870 表单规则优化，整数可以以0开头，以为.00结尾

--- src/views/baseSetting/formRules.js
+++ src/views/baseSetting/formRules.js
@@ -2,9 +2,9 @@ export default {
   chinese: {pattern: /^[\u4e00-\u9fa5]+$/g, message: '请输入汉字', trigger: 'change'},
   word_number: {pattern: /^\w+$/g, message: '请输入字母、数字'},
   float: {pattern: /^(([1-9]\d*)(\.\d{1,2})?|0\.[1-9]\d{0,1}|0\.0[1-9])$/g, message: '请输入最多两位小数，且不为0', trigger: 'change'}, //正整数或小数
-  number: {pattern: /^[1-9]\d*$/g, message: '请输入正整数', trigger: 'change'}, //正整数
+  number: {pattern: /^(([1-9]\d*)|(0*\d+)|(0*\d+\.0*))$/g, message: '请输入正整数', trigger: 'change'}, //正整数
   float2: {pattern: /^(([1-9]\d*)(\.\d{1,2})?|0\.\d{1,2})$/g, message: '请输入两位小数', trigger: 'change'}, //只能输入两位小数
-  number5: {pattern: /^[1-9]\d{0,3}$/g, message: '请输入范围1~9999的整数', trigger: 'change'}, //5代表整数部分不超过五位数
-  float5: {pattern: /^(([1-9]\d{0,3})(\.\d{1,2})?|0\.\d{1,2})$/g, message: '请输入范围0~9999的数', trigger: 'change'},//包括小数
+  number5: {pattern: /^([1-9]\d{0,3})|(0*[1-9]\d{0,3}|(0*\d+\.0*))$/g, message: '请输入范围1~9999的整数', trigger: 'change'}, //5代表整数部分不超过五位数
+  float5: {pattern: /^(([1-9]\d{0,3})(\.\d{1,2})?|(0\.\d{1,2}))$/g, message: '请输入范围0~9999的数', trigger: 'change'},//包括小数
   word_number_chinese: {pattern: /^[a-zA-Z0-9\u4e00-\u9fa5]+$/g, message: '请输入数字、字母、汉字'},
 };
