a65ab2eb404e71caf29142892d100371faaec23b
gw <2132>
2019-09-02 17:22:13 +0800

    add: 新增http报错提示

--- src/api/base-service/http/httpConfig.js
+++ src/api/base-service/http/httpConfig.js
@@ -5,9 +5,9 @@ var config = {
   onResponse(res) { //后端返回数据时执行（不管success时true还是false）
     return res;
   },
-  // onError(msg) { //请求没有连接上后端时执行
-  //   Message({message: msg, type: 'error', showClose: true});
-  // },
+  onError(data) { //请求没有连接上后端时执行
+    Message(Object.assign({}, {message: data.msg, type: 'error'}));
+  },
   onFailure(data) { //请求没有连接上后端和success为false时执行
     // Message(Object.assign({}, messageConfig, {message: msg, type: 'error'}));
     Message(Object.assign({}, {message: data.msg, type: 'error'}));
