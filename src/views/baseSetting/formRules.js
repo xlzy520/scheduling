233144bd2ed7af8b6c19dec2ff0545fff7828814
zhibi <1017866168@qq.com>
2019-09-04 15:39:55 +0800

    fix 修复小于9999的小数正则校验

--- src/views/baseSetting/formRules.js
+++ src/views/baseSetting/formRules.js
@@ -5,6 +5,6 @@ export default {
   number: {pattern: /^[0]*?([1-9]\d*)$/g, message: '请输入正整数', trigger: 'change'}, //正整数,不为0
   float2: {pattern: /^([0]*?(\.)?([1-9]\d*|0[1-9]))(\.\d{1,2})?$/g, message: '请输入两位小数', trigger: 'change'}, //只能输入两位小数
   number5: {pattern: /^[0]*?([1-9]\d{0,3})$/g, message: '请输入范围1~9999的整数', trigger: 'change'}, //5代表整数部分不超过五位数
-  float5: {pattern: /^(([1-9]\d{0,3})(\.\d{1,2})?|(0\.\d{1,2}))$/g, message: '请输入范围0~9999的小数', trigger: 'change'}, //包括小数
+  float5: {pattern: /^[0]*?(([1-9]\d{0,3})(\.\d{1,2})?|(0\.\d{1,2}))$/g, message: '请输入范围0~9999的小数', trigger: 'change'}, //包括小数
   word_number_chinese: {pattern: /^[a-zA-Z0-9\u4e00-\u9fa5]+$/g, message: '请输入数字、字母、汉字', trigger: 'change'},
 };
