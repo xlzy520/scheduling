722136a6d5487fe5d151d7b7dfb9d98961d2aff8
gw <2132>
2019-09-26 14:26:40 +0800

    fix: 12462 所有有切换页的弹框选项卡内容高度固定，超出滚动

--- src/mixins/dialogFixed.js
+++ src/mixins/dialogFixed.js
@@ -1,6 +1,16 @@
 /**
- * 用于固定弹框内容高度
+ * 用于固定弹框选项卡内的内容高度
  */
+import { methods } from "djweb";
+const { getStyle } = methods;
+function getElHeight(el) {
+  if (el) {
+    let height = parseFloat(getStyle(el, 'height'));
+    let top = parseFloat(getStyle(el, 'margin-top'));
+    let bottom = parseFloat(getStyle(el, 'margin-bottom'));
+    return height + top + bottom;
+  }
+}
 export default {
   data() {
     return {
@@ -11,23 +21,34 @@ export default {
       scrollbarView: undefined
     }
   },
+  mounted() {
+    this.fixedDialog();
+  },
   methods: {
     updateContentHeight() {
-      this.content_height = parseFloat(this.$method.getStyle(this.scrollbarView, 'height'))
+      this.content_height = parseFloat(getStyle(this.scrollbarView, 'height'))
     },
     calcHeight() {
       if (!this.content_height) {
         this.updateContentHeight();
       }
       let height_v = this.content_height;
-      let bottom_v = parseFloat(this.$method.getStyle(this.scrollbarWrap, 'margin-bottom'));
-      let maxHeight_w = parseFloat(this.$method.getStyle(this.scrollbarWrap, 'max-height'));
+      let bottom_v = parseFloat(getStyle(this.scrollbarWrap, 'margin-bottom'));
+      let maxHeight_w = parseFloat(getStyle(this.scrollbarWrap, 'max-height'));
+      let height_content;
       if (maxHeight_w > height_v) {
-        this.scrollbarWrap.style.height = (height_v - bottom_v) + 'px';
+        height_content = height_v - bottom_v;
+        // this.scrollbarWrap.style.height = (height_v - bottom_v) + 'px';
       } else {
-        this.scrollbarWrap.style.height = maxHeight_w + 'px';
+        height_content = maxHeight_w;
+        // this.scrollbarWrap.style.height = maxHeight_w + 'px';
       }
-      this.scrollbar.resize();
+      let elTabsContent = this.scrollbarView.querySelector('.el-tabs__content');
+      let elTabsHeader = this.scrollbarView.querySelector('.el-tabs__header');
+      elTabsContent.style.height = (height_content - getElHeight(elTabsHeader)) + 'px';
+      elTabsContent.style.overflow = 'auto';
+      this.scrollbarWrap.style.overflow = 'auto';
+      // this.scrollbar.resize();
     },
     fixedDialog() {
       setTimeout(() => {
