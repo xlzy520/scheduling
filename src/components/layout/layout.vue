380d2a8b08f0341512bcb35fa6b192ddc542b051
gw <2132>
2019-09-29 18:05:51 +0800

    del: 删除测试代码

--- src/components/layout/layout.vue
+++ src/components/layout/layout.vue
@@ -20,117 +20,75 @@
         </div>
       </div>
     </div>
-    <div class="abs-route-nav">
-      <router-link :to="route.path" v-for="route in routes" :key="route.name">{{route.meta.title}}</router-link>
-    </div>
   </div>
 </template>
 
 <script>
-  import tagsView from './tagsView';
-  import allRoutes from '../../router/permissionRouter';
-  import {mapGetters} from 'vuex';
-  export default {
-    name: 'layout',
-    components: {
-      tagsView,
-    },
-    data() {
-      return {
-        isShow: true,
-      };
-    },
-    computed: {
-      ...mapGetters([
-        'cachedViews',
-        'menus',
-      ]),
-      routes() {
-        function getPer(routes, menu) {
-          routes.forEach(route => {
-            menu.push(route);
-          });
-        }
+import { mapGetters } from 'vuex';
 
-        let baseRouteNameMap = ['baseSetting', 'paperStockManage', 'prodManage', 'statement'];
-        console.log(allRoutes);
-        let myRoutes = (baseRouteName) => allRoutes.filter(item=>
-          item.name.indexOf(`${baseRouteName}`) > -1 && item.name.indexOf(`*`) < 0);
-        let permissionMenu = [];
-        //如果是开发者状态，获取全部权限
-        if (process.env.NODE_ENV === 'development') {
-          getPer(myRoutes(baseRouteNameMap[0]), permissionMenu);
-          getPer(myRoutes(baseRouteNameMap[1]), permissionMenu);
-          getPer(myRoutes(baseRouteNameMap[2]), permissionMenu);
-          getPer(myRoutes(baseRouteNameMap[3]), permissionMenu);
-        }
-        return permissionMenu;
-      }
+export default {
+  name: 'layout',
+  data() {
+    return {
+      isShow: true
+    };
+  },
+  computed: {
+    ...mapGetters([
+      'cachedViews',
+      'menus'
+    ])
+  },
+  methods: {
+    refresh() {
+      this.isShow = false;
+      this.$nextTick(()=>{
+        this.isShow = true;
+      });
     },
-    methods: {
-      refresh() {
-        this.isShow = false;
-        this.$nextTick(() => {
-          this.isShow = true;
-        });
-      },
-      switchMenu(menu) {
-        this.$router.push(menu.path);
-      },
-    },
-    // destroyed() {
-    //   window.observer.remove('refresh');
-    // },
-    // mounted() {
-    //   window.observer.on('refresh', this.refresh);
-    // },
-  };
+    switchMenu(menu) {
+      this.$router.push(menu.path);
+    }
+  },
+  destroyed() {
+    window.subject.remove('refresh');
+  },
+  mounted() {
+    window.subject.listen('refresh', this.refresh);
+  }
+};
 </script>
 
 <style lang="less" scoped>
-  .abs-route-nav {
-    background-color: #ede4f5;
-    border-radius: 4px;
-    position: absolute;
-    top: 53px;
-    z-index: 2000;
+.layout {
+  height: 100%;
+  &-body {
+    position: relative;
     display: flex;
-    height: 50px;
-    line-height: 50px;
-    a {
-      margin-left: 20px;
-      color: deeppink;
-    }
-  }
-  .layout {
     height: 100%;
-    &-body {
+    &-menu {
+      position: absolute;
+      z-index: 200;
+      height: 100%;
+    }
+    &-content {
       position: relative;
       display: flex;
-      height: 100%;
-      &-menu {
-        position: absolute;
-        z-index: 2000;
-        height: 100%;
-      }
-      &-content {
+      width: 100%;
+      overflow: auto;
+      flex-grow: 1;
+      flex-direction: column;
+      background-color: #f4f4f4;
+      &-tags {
         position: relative;
-        display: flex;
-        width: 100%;
-        overflow: auto;
+        flex-shrink: 0;
+        height: 45px;
+      }
+      &-view {
+        position: relative;
         flex-grow: 1;
-        flex-direction: column;
-        background-color: #f4f4f4;
-        &-tags {
-          position: relative;
-          flex-shrink: 0;
-          height: 45px;
-        }
-        &-view {
-          position: relative;
-          flex-grow: 1;
-        }
       }
     }
   }
+}
 </style>
